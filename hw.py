ade658f537d65f278c3c606f956cbedd5b4bdb07
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2026-01-11 09:45:30 +0530

    ok

---
+++ hw.py
@@ -0,0 +1,6 @@
+base = int(input("Enter the base number: "))
+exponent = int(input("Enter the exponent: "))
+result = 1
+for i in range(exponent):
+    result *= base
+print(f"{base} to the power of {exponent} is {result}") 
